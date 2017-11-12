4697063b0a0d24e57a643d83372cc7df1ee53eb8
Marika Murphy <marikamurphy18@gmail.com>
2017-11-12 14:18:10 -0600

    add gitignore

--- kikaskitchen_homepage.php
+++ kikaskitchen_homepage.php
@@ -8,7 +8,9 @@
 		
 		  <meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
@@ -28,7 +30,7 @@
 		
 		<body
 		<?php
-		echo "<table border='1' width = '500' align = 'left' >";
+		echo "<table border='1' width = '500' align = 'right' >";
 		
 			
 				
